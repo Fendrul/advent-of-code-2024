fda0c15a841db3a52fddc8621132cdae3084483c
Simon <simon.strykers@gmail.com>
2024-12-02 11:57:49 +0100

    Solutions for day 2

---
+++ src/day-2/src/entities/report.rs
@@ -0,0 +1,86 @@
+use Safetyness::{Safe, Unsafe};
+use std::cmp::PartialEq;
+
+#[derive(Debug)]
+pub struct Report {
+    numbers: Vec<i32>,
+    safetyness: Safetyness,
+}
+
+impl Report {
+    pub fn new(numbers: Vec<i32>) -> Report {
+        Report {
+            safetyness: give_safetyness(&numbers),
+            numbers,
+        }
+    }
+
+    pub fn get_safetyness(&self) -> &Safetyness {
+        &self.safetyness
+    }
+
+    pub fn get_safetyness_with_tolerance(&self) -> Safetyness {
+        match self.get_safetyness() {
+            Safe => Safe,
+            Unsafe => {
+                for i in 0..self.numbers.len() {
+                    let mut current_vec = self.numbers.clone();
+                    current_vec.remove(i);
+                    
+                    if Safe == give_safetyness(&current_vec) {
+                        return Safe;
+                    }
+                }
+                
+                Unsafe
+            }
+        }
+    }
+}
+
+fn give_safetyness(slice: &[i32]) -> Safetyness {
+    let interval = 1;
+
+    let mut global_direction: Option<Direction> = None;
+
+    for (current, next) in slice.iter().zip(slice.iter().skip(interval)) {
+        global_direction = if global_direction.is_some() {
+            global_direction
+        } else {
+            Some(Direction::from(*current, *next))
+        };
+
+        if global_direction != Some(Direction::from(*current, *next)) {
+            return Unsafe;
+        }
+
+        let diff = (current - next).abs();
+        if diff > 3 || diff == 0 {
+            return Unsafe;
+        }
+    }
+
+    Safe
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Direction {
+    Increasing,
+    Decreasing,
+}
+
+impl Direction {
+    fn from(x: i32, y: i32) -> Direction {
+        if x < y {
+            Direction::Increasing
+        } else {
+            Direction::Decreasing
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Safetyness {
+    Safe,
+    Unsafe,
+}
---
+++ src/day-2/src/main.rs
@@ -0,0 +1,85 @@
+#![feature(let_chains)]
+#![feature(assert_matches)]
+
+mod entities;
+
+use crate::entities::report::{Report, Safetyness};
+use file_reader::file_reader::FileReader;
+use std::error::Error;
+use std::time::Instant;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let timer = Instant::now();
+    let path_to_file = "src/day-2/src/input";
+
+    let (number_safe_report, number_safe_report_with_tolerance) = solve(path_to_file)?;
+
+    println!("Number of safe reports: {}", number_safe_report);
+    println!(
+        "Number of safe reports with tolerance: {}",
+        number_safe_report_with_tolerance
+    );
+
+    println!("Finished in: {}µs", timer.elapsed().as_micros());
+    Ok(())
+}
+
+fn solve(path_to_file: &str) -> Result<(i32, i32), Box<dyn Error>> {
+    let vec_report = FileReader::new(path_to_file)?
+        .map(|line| {
+            let numbers = line
+                .split_whitespace()
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>();
+
+            Report::new(numbers)
+        })
+        .collect::<Vec<_>>();
+
+    let number_safe_report = vec_report
+        .iter()
+        .map(|report| match report.get_safetyness() {
+            Safetyness::Safe => 1,
+            Safetyness::Unsafe => 0,
+        })
+        .sum::<i32>();
+
+    let number_safe_report_with_tolerance = vec_report
+        .iter()
+        .map(|report| match report.get_safetyness_with_tolerance() {
+            Safetyness::Safe => 1,
+            Safetyness::Unsafe => 0,
+        })
+        .sum::<i32>();
+
+    Ok((number_safe_report, number_safe_report_with_tolerance))
+}
+
+#[cfg(test)]
+mod test {
+    use crate::entities::report::Safetyness::Safe;
+    use super::*;
+
+    #[test]
+    fn test_main() -> Result<(), Box<dyn std::error::Error>> {
+        let (result, result_with_tolerance) = solve("src/day-2/src/test")?;
+
+        assert_eq!(2, result);
+        assert_eq!(4, result_with_tolerance);
+
+        let (result, result_with_tolerance) = solve("src/day-2/src/input")?;
+
+        assert_eq!(390, result);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_safetyness() {
+        let report = Report::new(vec![8, 6, 4, 4, 1]);
+
+        let safetyness = report.get_safetyness_with_tolerance();
+
+        assert_eq!(safetyness, Safe);
+    }
+}
